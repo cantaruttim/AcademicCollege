4b8a5f860f1b32e9fef787355a265cfebe0efd33
cantaruttim <cantaruttim@outlook.com>
2025-09-29 23:33:57 -0300

    initializing project

---
+++ src/test/java/br/com/college/AcademicCollegeTests.java
@@ -0,0 +1,13 @@
+package br.com.college;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicCollegeTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
